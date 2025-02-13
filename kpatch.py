7cdc48d764c251a91f1e3cf21e23fc0f58efdf0e
Radomir Vrbovsky <rvrbovsk@redhat.com>
2025-02-13 21:13:17 +0100

    kpatch: Add filter logic using the sack callback
    Add filter logic to the DNF plugin. If enabled, use sack() callback of
    the DNF plugin to show kernels supported by kpatch.
    
    V2:
    * Added BOC messages
    
    Based-on-code-by: Marek Blaha mblaha@redhat.com
    Based-on-code-by: Joe Lawrence jolawren@redhat.com
    Signed-off-by: Radomir Vrbovsky <rvrbovsk@redhat.com>

--- kpatch.py
+++ kpatch.py
@@ -27,10 +27,12 @@ import re
 
 from dnfpluginscore import _, logger
 
+import dnf
 import dnf.callback
 import dnf.cli
 import dnf.exceptions
 import dnf.transaction
+import hawkey
 
 KPATCH_PLUGIN_NAME = "kpatch"
 KPATCH_UPDATE_OPT = "autoupdate"
@@ -211,6 +213,10 @@ class KpatchPlugin(dnf.Plugin):
 
     name = KPATCH_PLUGIN_NAME
 
+    # list of package names to filter based on kpatch support
+    kernel_pkg_names = ['kernel', 'kernel-core', 'kernel-modules',
+                        'kernel-modules-core', 'kernel-modules-extra']
+    kpatch_requirement = ['kernel', 'kernel-uname-r']
 
     def __init__(self, base, cli):
         super(KpatchPlugin, self).__init__(base, cli)
@@ -240,6 +246,48 @@ class KpatchPlugin(dnf.Plugin):
         self.base.resolve(self.cli.demands.allow_erasing)
         self._commiting = False
 
+
+    def sack(self):
+        if not self._autofilter:
+            return
+
+        print('Please note, kpatch filter is enabled, only kpatch supported kernels are shown.')
+
+        # This query gradually accumulates all kernel packages that should be
+        # offered to the user (kernels for which exists kpatch-patch-* package
+        # that requires it). Start with empty query.
+        kernels_keep = self.base.sack.query().filterm(empty=True)
+
+        # pre-filter all available versions of the kernel* packages
+        kernels_query = self.base.sack.query(flags=hawkey.IGNORE_EXCLUDES)
+        kernels_query.filterm(name=self.kernel_pkg_names)
+        # any installed kernel version should not be excluded
+        kernels_query = kernels_query.available()
+
+        # Add to the kernels_keep query all kernel-core package versions that are
+        # required by any of kpatch-patch-* packages.
+        kpatch_query = self.base.sack.query(flags=hawkey.IGNORE_EXCLUDES)
+        kpatch_query.filterm(name__glob="kpatch-patch-*")
+        for kpatch_pkg in kpatch_query:
+            for require in kpatch_pkg.requires:
+                require_parsed = str(require).split(' ')
+                if len(require_parsed) < 3:
+                    continue
+                if require_parsed[0] in self.kpatch_requirement:
+                    # get kernel-core package providing "kernel-uname-r = <kpatch_pkg.evra>"
+                    kernel_core = kernels_query.filter(provides=require)
+                    kernel_evr = None
+                    for kernel_core_pkg in kernel_core:
+                        # assume that all such packages have the same evr
+                        kernel_evr = kernel_core_pkg.evr
+                        break
+                    if kernel_evr is not None:
+                        kernels_keep = kernels_keep.union(kernels_query.filter(evr=kernel_evr))
+                    # assume the is only one kernel-uname-r requirement
+                    break
+
+        # exclude all kernel-core packages that are not in kernels_keep query
+        self.base.sack.add_excludes(kernels_query.difference(kernels_keep))
 
     def resolved(self):
         # Calling self.base.resolve() will run this callback again
